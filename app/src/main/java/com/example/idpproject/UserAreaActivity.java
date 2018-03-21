38c5fe0009aa0d925456b9577203987752368139
ken627 <lawkinping@gmail.com>
2018-03-21 22:48:52 +0800

    menu
    added menu

--- app/src/main/java/com/example/idpproject/UserAreaActivity.java
+++ app/src/main/java/com/example/idpproject/UserAreaActivity.java
@@ -1,13 +1,34 @@
 package com.example.idpproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class UserAreaActivity extends AppCompatActivity {
+    EditText tvWelcomeMsg, etUserName;
+    Button bNext;
+    TextView tvUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
+
+        //create obj
+        bNext = (Button) findViewById(R.id.bNext);
+
+        // Change to Main Activiity
+        bNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Next = new Intent(UserAreaActivity.this,MainFunction.class);
+                startActivity(Next);
+            }
+        });
+
     }
 }
